f01b747bdee67964b17ac62563b9b02ea85cebee
Suganthan <suganthan0001@gmail.com>
2024-03-30 14:33:39 +0530

    login page ui finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import PageNotFound from "./components/PageNotFound";
+import Login from "./components/loginPage/Login";
+import { Route,Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [theme, setTheme] = useState(true);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div data-theme={theme ? "light" : "dark"} className="duration-300">
+      <Routes>
+        <Route path="/login" element={<Login setTheme = {setTheme} theme={theme} />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function PageNotFound() {
+  return (
+    
+    <>
+        <h1>PAGE NOT FOUNT</h1>
+        <Link to="/login">HOME</Link>
+    </>
+  )
+}
+
+export default PageNotFound
